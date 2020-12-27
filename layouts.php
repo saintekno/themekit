d3d71c905516407dde22f13ddf455a57053343ed
buddywinangun <buddywinangun@gmail.com>
2020-12-27 15:55:47 +0700

    upgrade role

---
+++ layouts.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<?php $this->load->frontend_view('partials/header.php');?>
+
+<?php $this->load->frontend_view('pages/'.$pages.'.php');?>
+  
+<?php $this->load->frontend_view('partials/footer.php');?>
