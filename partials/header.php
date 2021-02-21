ae05f0863e3beb1294dc683ed8244c7a20f5b678
buddywinangun <buddywinangun@gmail.com>
2021-02-21 15:48:48 +0700

    fixed variable options

--- partials/header.php
+++ partials/header.php
@@ -1,23 +1,24 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+global $Options;
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html class="no-js" lang="en">
 
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="<?php echo $this->Options['site_description'];?>" />
-        <meta name="keywords" content="Saas, Software, multi-uses, HTML, Clean, Modern" />
+        <meta name="description" content="<?php echo $Options['site_description'];?>" />
+        <meta name="keywords" content="<?php echo $Options['site_keywords'];?>" />
         <meta name="author" content="<?php echo theme_config('author');?>" />
         <meta name="email" content="<?php echo theme_config('email');?>" />
         <meta name="website" content="<?php echo site_url();?>" />
         <meta name="Version" content="<?php echo theme_config('version');?>" />
         
         <!-- Site Title -->
-        <title><?php echo $this->Options['site_name']; ?></title>
+        <title><?php echo $Options['site_name']; ?> - <?php echo $Options['site_title']; ?></title>
 
         <!-- favicon -->
         <link rel="shortcut icon" type="image/ico" href="<?php echo base_url('uploads/system/'.(!empty($Options['favicon']) ? $Options['favicon'] : 'favicon.png'));?>">
